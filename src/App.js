0a8dbed5f7aa37200c2c5f3914181bcc7963496d
Jai Gaur <jaigaur2609@gmail.com>
2021-06-08 22:36:00 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hey Jai Gaur! Let's build netflix clone front-end today</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/requests.js
@@ -0,0 +1,1 @@
+const API_KEY = "be9f6f5d5e37b627e53160c23e79fea2";
