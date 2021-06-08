3cb6f0731397d67881ef4005105c89032be5a819
Jai Gaur <jaigaur2609@gmail.com>
2021-06-09 00:04:01 +0530

    add to axios.js

---
+++ src/axios.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+//create() is provided by axios
+//base URL to make requests to the movie database
+const instance = axios.create({
+    baseURL: "https://api.themoviedb.org/3",
+});
+
+//every instance that we create will be appended to the baseURL created above
+instance.get('/foo-bar');//this is an instance. the url would look like https://api.themoviedb.org/3/foo-bar
+
+export default instance;
