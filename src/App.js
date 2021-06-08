48a33acd90836bac11c3d8002a3abb815a7907f1
Jai Gaur <jaigaur2609@gmail.com>
2021-06-09 00:22:26 +0530

    create Row.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Row from './Row';
 
 function App() {
   return (
     <div className="App">
       <h1>Hey Jai Gaur! Let's build netflix clone front-end today</h1>
+      //below are the different rows corresponding to the different sections of clone webpage
+      <Row title="NETFLIX ORIGINALS" />
+      <Row title="Trending Now" />
+      
     </div>
   );
 }
---
+++ src/Row.js
@@ -0,0 +1,14 @@
+//type 'rfce' to get the basic react layout
+import React from 'react'
+
+function Row() {
+    return (
+        <div>
+            {/* title */}//this will have titles like 'trending now' 'top rated' etc
+
+            {/* container -> posters */}//this will have multiple posters under various titles 
+        </div>
+    )
+}
+
+export default Row
