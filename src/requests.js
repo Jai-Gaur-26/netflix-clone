71a348c958a9be561021a5e556bb15fe01f10176
Jai Gaur <jaigaur2609@gmail.com>
2021-06-08 22:53:22 +0530

    Add to requests.js

--- src/requests.js
+++ src/requests.js
@@ -1,1 +1,14 @@
 const API_KEY = "be9f6f5d5e37b627e53160c23e79fea2";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&langage=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with-genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with-genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with-genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with-genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with-genres=99`,
+}
+
+export default requests;
